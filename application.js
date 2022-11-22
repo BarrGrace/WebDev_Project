5d065bc2d84029dafcebc283a6455b19f8fae23d
Barr Grace <barr.grace@grunitech.com>
2022-11-22 22:27:46 +0200

    updates

--- application.js
+++ application.js
@@ -1,31 +1,124 @@
-function darkModeOn(){
+const colourSwitch = document.getElementById("input-colour-switch");
+colourSwitch.addEventListener("click", checkMode);
 
-    document.body.classList.add("dark-mode");
+document.getElementById("operationLog").style.display = "none";
+document.getElementById("history").style.display = "none";
+document.body.classList.add("hide-sicence-grid");
+
+document.getElementById("sci").addEventListener('click', sci_hidden);
+
+let darkMode = true;
+let show = true;
+let sciShow = true;
+
+function info(){
+
+    alert("Developer: Barr Grace\nCalculator Version: 1.0.0\nShort Description: This app will alow you to calculate.");
 }
-
-function darkModeOff(){
-
-    document.body.classList.remove("dark-mode");
-}
-
-
 function checkMode() {
 
     if (darkMode){
 
-        darkModeOn();
+        document.getElementById('screen').style.backgroundColor = 'yellow';
+        document.getElementById('input-colour-switch').style.backgroundColor= 'orange';
     }
-
     else {
 
-        darkModeOff();
+        document.getElementById('screen').style.backgroundColor = '';
+        document.getElementById('input-colour-switch').style.backgroundColor = '';
     }
-
     darkMode = !darkMode;
 }
+function show_hidden() {
 
-const colourSwitch = document.getElementById("input-colour-switch");
-colourSwitch.addEventListener("click", checkMode);
+    if (show){
 
+        document.getElementById("operationLog").style.display = "flex";
+        document.getElementById("history").style.display = "flex";
+        document.getElementById("modes").style.backgroundColor = "orange";
+        
+    }
+    else {
 
-let darkMode = true;
+        document.getElementById("operationLog").style.display = "none";
+        document.getElementById("history").style.display = "none";
+        document.getElementById("modes").style.backgroundColor = "";
+    }
+    show = !show;
+}
+function sci_hidden() {
+
+    if (sciShow){
+
+        document.body.classList.remove("hide-sicence-grid");
+        document.getElementById('sci').style.backgroundColor = "orange";
+    }
+    else {
+        
+        document.body.classList.add("hide-sicence-grid");
+        document.getElementById('sci').style.backgroundColor = "";
+    }
+    sciShow = !sciShow;
+}
+
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+
+let M = urlParams.get('Mode');
+let colour = urlParams.get('colour');
+let font = urlParams.get('font');
+
+if (font == 'italic') {
+
+    document.body.style.fontStyle = 'italic';
+}
+else if (font == 'Lucida Sans') {
+
+    document.body.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+}
+else if (font == 'Courier New'){
+
+    document.body.style.fontFamily = "'Courier New', Courier, monospace";
+}
+else if (font = 'Georgia') {
+
+    document.body.style.fontFamily = "Georgia, 'Times New Roman', Times, serif"
+}
+else {
+
+    document.body.style.fontStyle = 'normal';
+}
+
+if (colour == 'gray') {
+
+    document.body.classList.add('gray')
+}
+else if (colour == 'blue') {
+
+    document.body.classList.add('blue');
+}
+else if (colour == 'red') {
+
+    document.body.classList.add('red')
+}
+else if (colour == 'green') {
+
+    document.body.classList.add('green')
+}
+else if (colour == 'purple') {
+
+    document.body.classList.add('purple');
+}
+else {
+
+    document.body.classList.add('normal');
+}
+
+if (M == 'on'){
+
+    document.body.classList.add('dark-mode');
+}
+else {
+
+    document.body.classList.remove('dark-mode');
+}
--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-function calculate(count) {
+function sciCalculate(count) {
 
     index = count.indexOf("+");
 
@@ -8,11 +8,11 @@ function calculate(count) {
 
             return parseFloat(count);
         }
-
+        
         let s1 = count.substring(0, index);
         let s2 = count.substring(index + 1);
 
-        return calculate(s1) + calculate(s2);
+        return sciCalculate(s1) + sciCalculate(s2);
     }
 
     index = count.indexOf("-");
@@ -27,7 +27,7 @@ function calculate(count) {
         let s1 = count.substring(0, index);
         let s2 = count.substring(index + 1);
 
-        return calculate(s1) - calculate(s2);
+        return sciCalculate(s1) - sciCalculate(s2);
     }
 
     index = count.indexOf("X");
@@ -37,7 +37,7 @@ function calculate(count) {
         let s1 = count.substring(0, index);
         let s2 = count.substring(index + 1);
 
-        return calculate(s1) * calculate(s2);
+        return sciCalculate(s1) * sciCalculate(s2);
     }
 
     index = count.indexOf("/");
@@ -47,57 +47,267 @@ function calculate(count) {
         let s1 = count.substring(0, index);
         let s2 = count.substring(index + 1);
 
-        return calculate(s1) / calculate(s2);
+        return sciCalculate(s1) / sciCalculate(s2);
     }
 
     return parseFloat(count);
 }
 
+let current = "0";
+let operaotr = "";
+let operand1 = "";
+let operand2 = "";
+let display = "0";
+let first_operand = true;
+let sciMode = false;
+let history = [''];
+let temp;
+let sci = [''];
 
+document.getElementById('screen').innerHTML = current;
 
-function info(){
+function reset() {
 
-    alert("Developer: Barr Grace\nCalculator Version: 1.0.0\nShort Description: This app will alow you to calculate.");
+    current = '0';
+    display = '0';
+    operand1 = '';
+    operand2 = '';
+    operaotr = '';
+    first_operand = true;
+    history = [''];
+    document.getElementById("history").innerHTML = history;
 }
+function press(element){
 
-function press(number){
-    
+    if (isDigit(element) || isOperator(element)){
 
-    if (restart || count == '0'){
-
-        count = '';
-        restart = false;
+        history.push(element);
+        sci.push(element);
     }
-    
-    if (number == 'C'){
 
-        count = '0'
-    }
-    else if (number == '='){
+    if (isDigit(element)){
 
-        count = calculate(count) + "";
-        restart = true;
+        if (first_operand){
 
-    }
-    else if (number == "arrow"){
+            operand1 += element;
+            display = operand1;
+        }
+        else{
 
-        count = count.slice(0, count.length - 1);
-
-        if (count == '') {
-
-            count = '0';
+            operand2 += element;
+            display = operand2;
         }
     }
-    else {
 
-        count = count + number;
+    else if (isOperator(element)){
+
+        if (operand2 != ''){
+
+            operand1 = calculate(operand1, operaotr, operand2) + "";
+        }        
+        operaotr = element;
+        display = element;
+        first_operand = false;
+        operand2 = '';
     }
 
+    else if (element == '='){
     
-    document.getElementById("screen").innerHTML = count;
+        if (operand1 != '' && operand2 != ''){
+
+            current = calculate(operand1, operaotr, operand2) + "";
+        }
+        else if (operand1 == '') {
+
+            current = calculate('0', operaotr, operand2) + "";
+        }
+        scienceExpression(current);
+
+        display = current;
+        operand1 = current;
+        operand2 = '';
+        first_operand = true;
+
+        if (sciMode) {
+
+            current = sciCalculate(sci.join(''));
+            current = current.toExponential(4);
+            display = current;
+        }
+
+        history.push('<br>' + '=' + current + '<br>');
+        document.getElementById("history").innerHTML = history.join('');
+        history.push(current);
+
+        // alert(current);
+    }
+
+    else if (element == 'C'){
+
+        reset();
+    }
+
+    else if (element == 'arrow'){
+
+        history.pop()
+
+        if (display == operand2){
+
+            operand2 = '';
+            display = operaotr;
+        }
+        else if (display == operaotr){
+
+            operaotr = '';
+            display = operand1;
+        }
+        else{
+
+            operand1 = '';
+            operand2 = '';
+            display = '0'
+            first_operand = true;
+        }
+    }
+
+    else if (element == "PM") {
+
+        if (operand1 == '' && operand2 == ''){
+
+            return;
+        }
+
+        if (first_operand) {
+
+            operand1 = (parseFloat(operand1) * -1) + '';
+            display = operand1;
+        }
+
+        else if (display == current) {
+
+            current = (parseFloat(current) * -1) + '';
+            display = current;
+        }
+
+        else if (!isOperator(history[history.length - 1])) {
+
+            operand2 = (parseFloat(operand2) * -1) + '';
+            display = operand2;
+        }
+
+        changeHistory();
+    }
+
+    else if (element == "sci") {
+
+        // reset(); //Part 4 => Task 2 => b.
+        sciOff();
+        sciMode = !sciMode;
+    }
+
+    document.getElementById('screen').innerHTML = display;
 }
 
+function changeHistory() {
 
-let count = "0";
-let restart = true;
-document.getElementById("screen").innerHTML = count;
+    let i = history.length - 1;
+
+    if (isOperator(history[i])) {
+
+        return;
+    }
+    while (i > 0) {
+
+        if (!isDigit(history[i]) && !negDigit(history[i])){
+
+            break;
+        }
+
+        i--;
+    }
+    history[i + 1] = (-1 * parseFloat(history[i + 1])) + '';
+}
+
+function negDigit(element) {
+
+    let lst = ['-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9'];
+
+    for (let i = 0; i < 9; i++) {
+
+        if (element == lst[i]) {
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function sciOff() {
+
+    let temp = Number(parseFloat(current));
+
+    if (history[history.length - 1] == current) {
+
+        history[history.length - 1] = temp;
+    }
+
+    current = temp;
+}
+
+function calculate(operand1, operaotr, operand2){
+
+
+    if (operaotr == "X"){
+
+        return parseFloat(operand1) * parseFloat(operand2);
+    }
+    else if (operaotr == '/'){
+
+        return parseFloat(operand1) / parseFloat(operand2);
+    }
+    else if (operaotr == '+'){
+
+        return parseFloat(operand1) + parseFloat(operand2);
+    }
+
+    return parseFloat(operand1) - parseFloat(operand2);
+}
+
+function isOperator(operator){
+
+    operators = "-+X/";
+
+    for (let i = 0; i < operators.length; i++){
+
+        if (operator == operators.charAt(i)){
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isDigit(digit){
+
+    digits = "0123456789";
+
+    for (let i = 0; i < digits.length; i++){
+
+        if (digit == digits.charAt(i)){
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function scienceExpression(number) {
+
+    if (sciMode) {
+
+        current =  parseFloat(number).toExponential(4);
+    }
+}
